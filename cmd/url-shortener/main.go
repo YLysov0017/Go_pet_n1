ca069a191987161d8fa322c99361b8ff045a658c
YLysov0017 <yarlysov0117@gmail.com>
2024-12-02 15:20:50 +1000

    sqlite3 init

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,20 +2,69 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 
 	"github.com/YLysov0017/go_pet_n1/internal/config"
+	"github.com/YLysov0017/go_pet_n1/internal/config/storage/sqlite"
+	"github.com/YLysov0017/go_pet_n1/internal/lib/logger/sl"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
+	log := setupLogger(cfg.Env)
+
+	log.Info("service started", slog.String("Env", cfg.Env))
+	log.Debug("debug enabled")
+
+	var storPath string
+	switch runtime.GOOS {
+	case "windows":
+		mydir, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		storPath = strings.ReplaceAll(strings.Replace(cfg.StoragePath, ".", "", 1), "/", "\\") // Fixin' Windows filepath
+		storPath = filepath.Join(mydir, "..\\..", storPath)
+	default:
+		storPath = cfg.StoragePath // Unix path remain unchanged
+	}
+
 	fmt.Println(cfg)
 
-	// TODO: init logger: slog
-
-	// TODO: init storage: sqlite
+	storage, err := sqlite.New(storPath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	_ = storage
 
 	// TODO: init router: chi, chi render
 
 	// TODO: run server
+}
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func setupLogger(env string) *slog.Logger { // берем настройки из окружения
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+
 }
---
+++ internal/config/storage/sqlite/sqlite.go
@@ -0,0 +1,40 @@
+package sqlite
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3" // init driver
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(storagePath string) (*Storage, error) {
+	const op = "storage.sqlite.New"
+
+	db, err := sql.Open("sqlite3", storagePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err) // ошибка случилась при создании соединения с sqlite
+	}
+
+	stmt, err := db.Prepare(`
+	CREATE TABLE IF NOT EXISTS url(
+		ID INTEGER PRIMARY KEY,
+		alias TEXT NOT NULL UNIQUE,
+		url TEXT NOT NULL);
+	CREATE INDEX IF NOT EXISTS idx_alias ON url(alias);
+	`)
+
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Storage{db: db}, nil
+}
